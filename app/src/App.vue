8610035f17652ad62e755638303462d1b97d4625
AnsellC <ansellcruz@gmail.com>
2022-02-15 07:20:12 +0800

    layout

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,32 +1,13 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div class="bg-indigo-800 p-3 text-center mb-16">
+            <h1 class="text-3xl font-bold text-slate-50">
+                Aloware Test Project
+            </h1>
+        </div>
+        <div class="container mx-auto bg-white p-8">
+            <router-view />
+        </div>
+        <message-box></message-box>
     </div>
-    <router-view/>
-  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
