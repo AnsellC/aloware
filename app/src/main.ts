fedd78fa3e0cc63328d99a9a8cd2d0d9ec3f1d42
AnsellC <ansellcruz@gmail.com>
2022-02-15 07:18:41 +0800

    add auto registration of component

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import { upperFirst, camelCase } from 'lodash';
+import './filters';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+const requireComponent = require.context(
+    './components',
+    true,
+    /[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach((fileName) => {
+    const componentConfig = requireComponent(fileName);
+    const componentName = upperFirst(
+        camelCase(
+            fileName
+                .split('/')
+                .pop()
+                ?.replace(/\.\w+$/, '')
+        )
+    );
+
+    Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount('#app');
