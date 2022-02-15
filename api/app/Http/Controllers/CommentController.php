b731d78f15b4be43887ec251b3520a1dff35ec91
AnsellC <ansellcruz@gmail.com>
2022-02-16 00:27:54 +0800

    add list comment route

--- api/app/Http/Controllers/CommentController.php
+++ api/app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 
 
@@ -29,5 +30,13 @@ class CommentController extends Controller
 
 
         return response()->json(['message' => 'Comment created successfully', 'data' => $comment], 201);
+    }
+
+    public function index()
+    {
+        // Fetch all comments. Since there is no post to associate these comments with, we do not eager load anything. 
+        // The nesting mechanism will be handled by the frontend.
+
+        return CommentResource::collection(Comment::all());
     }
 }
