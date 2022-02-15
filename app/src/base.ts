6c5811f3e34d2e024604a5fde4bd24bc889d3bd4
AnsellC <ansellcruz@gmail.com>
2022-02-16 00:24:36 +0800

    add config file

--- app/src/base.ts
+++ app/src/base.ts
@@ -1,6 +1,7 @@
 import { Component, Vue } from 'vue-property-decorator';
-
+import config from '../config.json';
 @Component
 export default class BaseClass extends Vue {
     loading = false;
+    config = config;
 }
